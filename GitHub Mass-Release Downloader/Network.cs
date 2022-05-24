b5dc78469c6c24d49ac474ab90a0a02d2679da94
gnmmarechal <mliberato@gs2012.xyz>
2022-05-24 16:40:48 +0100

    Add project files.

---
+++ GitHub Mass-Release Downloader/Network.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Mass_Release_Downloader
+{
+    public class Network
+    {
+        public static string DownloadString(string URL)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var productValue = new ProductInfoHeaderValue("GitHubReleaseDownloader", "1.0");
+                var commentValue = new ProductInfoHeaderValue("(+https://gs2012.xyz)");
+
+                client.DefaultRequestHeaders.UserAgent.Add(productValue);
+                client.DefaultRequestHeaders.UserAgent.Add(commentValue);
+                using (var result = client.GetStringAsync(URL))
+                {
+                    result.Wait();
+                    return result.Result; 
+
+
+                }
+            }
+        }
+
+        public static void DownloadFile(string downloadUrl, string filePath)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(downloadUrl, filePath);
+        }
+    }
+}
---
+++ GitHub Mass-Release Downloader/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GitHub_Mass_Release_Downloader;
+using Newtonsoft.Json;
+
+Console.WriteLine("GitHub Release Downloader");
+
+int page = 1;
+List<Root> responses = new List<Root>();
+try
+{
+    while(true)
+    {
+        string response = Network.DownloadString("https://api.github.com/repos/Atmosphere-NX/atmosphere/releases?per_page=100&page=" + page++);
+        var githubResponse = JsonConvert.DeserializeObject<List<Root>>(response);
+        foreach (var item in githubResponse)
+            responses.Add(item);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Pages: " + (page-1));
+}
+if (responses.Count > 0)
+{
+    File.WriteAllText("test.txt", JsonConvert.SerializeObject(responses));
+}
+else
+{
+    Console.WriteLine("Loading from file...");
+    responses = JsonConvert.DeserializeObject<List<Root>>(File.ReadAllText("test.txt"));
+}
+
+Console.WriteLine();
+if (!Directory.Exists("downloads"))
+    Directory.CreateDirectory("downloads");
+Directory.SetCurrentDirectory("downloads");
+
+
+foreach (var item in responses)
+{
+    Console.WriteLine(item.tag_name);
+    Directory.CreateDirectory(item.tag_name);
+    foreach (var asset in item.assets)
+    {
+        Network.DownloadFile(asset.browser_download_url, item.tag_name + Path.DirectorySeparatorChar + asset.name);
+
+        Console.WriteLine(asset.browser_download_url);
+    }
+}
